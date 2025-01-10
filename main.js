9af4213ce6d506f03b9ee024d703a4aee3161c90
WebGameCreator <138807724+WebGameCreator@users.noreply.github.com>
2025-01-10 16:23:00 +0100

    Fixed bug

--- main.js
+++ main.js
@@ -16,17 +16,19 @@ let container;
 let camera, scene, renderer;
 let controller;
 
-let reticle;
-
+let pinching = false;
+const shapeSelector = document.getElementById("shapeSelector");
 let hitTestSource = null;
 let hitTestSourceRequested = false;
 let planeFound = false;
 let flowersGltf;
+let reticleVisible;
+let reticleMatrix;
 
+const drawingLines = [];
 let drawingPoints = [];
 let drawingMaterial;
 let drawingGeometry;
-let drawingLine;
 
 // check for webxr session support
 if ("xr" in navigator) {
@@ -59,11 +61,7 @@ const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
 light.position.set(0.5, 1, 0.25);
 scene.add(light);
 
-drawingPoints = [];
-drawingMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
-drawingGeometry = new THREE.BufferGeometry().setFromPoints(drawingPoints);
-drawingLine = new THREE.Line(drawingGeometry, drawingMaterial);
-scene.add(drawingLine);
+
 
 renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -75,44 +73,23 @@ container.appendChild(renderer.domElement);
 
 renderer.xr.addEventListener("sessionstart", sessionStart);
 
-document.body.appendChild(
+document.getElementById("controls").appendChild(
     ARButton.createButton(renderer, {
         requiredFeatures: ["local", "hit-test", "dom-overlay", "hand-tracking"],
         domOverlay: { root: document.querySelector("#overlay") },
     })
 );
 
-function onSelect() {
-    if (reticle.visible && flowersGltf) {
-        //pick random child from flowersGltf
-        const flower =
-            flowersGltf.children[
-            Math.floor(Math.random() * flowersGltf.children.length)
-            ];
-        const mesh = flower.clone();
-
-        reticle.matrix.decompose(mesh.position, mesh.quaternion, mesh.scale);
-        scene.add(mesh);
-    }
-}
-
 controller = renderer.xr.getController(0);
-controller.addEventListener("select", onSelect);
 scene.add(controller);
-
-reticle = new THREE.Mesh(
-    new THREE.RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2),
-    new THREE.MeshBasicMaterial()
-);
-reticle.matrixAutoUpdate = false;
-reticle.visible = false;
-scene.add(reticle);
 
 //load flowers.glb
 const loader = new GLTFLoader();
 
-loader.load("donut.glb", (gltf) => {
-    flowersGltf = gltf.scene;
+flowersGltf = await new Promise((resolve) => {
+    loader.load("donut.glb", (gltf) => {
+        resolve(gltf.scene);
+    });
 });
 
 window.addEventListener("resize", onWindowResize);
@@ -120,18 +97,7 @@ window.addEventListener("resize", onWindowResize);
 
 const hand = renderer.xr.getHand(0);
 
-hand.addEventListener('selectstart', () => console.log("selectstart"));
-hand.addEventListener('selectend', () => console.log("selectend"));
-hand.addEventListener('squeezestart', () => console.log("squeezestart"));
-hand.addEventListener('squeezeend', () => console.log("squeezeend"));
 scene.add(hand);
-
-const pivot = new THREE.Mesh(new THREE.IcosahedronGeometry(0.01, 3));
-pivot.name = 'pivot';
-pivot.position.z = - 0.05;
-
-hand.add(pivot);
-
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -143,57 +109,45 @@ function onWindowResize() {
 let lastPosition = {x: 0, y: 0, z: 0};
 
 renderer.setAnimationLoop((timeStamp, frame) => {
-    if (hand.joints?.["index-finger-tip"]?.position != null) {
-        const x = hand.joints["index-finger-tip"].position.x;
-        const y = hand.joints["index-finger-tip"].position.y;
-        const z = hand.joints["index-finger-tip"].position.z;
-        if (x != lastPosition.x || y != lastPosition.y || z != lastPosition.z) {
-            console.log(hand);
-            drawingPoints.push(new THREE.Vector3(x, y, z));
-            drawingGeometry.dispose();
-            drawingGeometry = new THREE.BufferGeometry().setFromPoints(drawingPoints);
-            drawingLine.geometry = drawingGeometry;
-            lastPosition = {x, y, z};
-        }
-    }
+    if (hand.inputState.pinching == true) {
+        if (shapeSelector.selectedIndex === 0) {
+            if (pinching == false) {
+                pinching = true;
+                drawingPoints = [];
+                drawingMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
+                drawingGeometry = new THREE.BufferGeometry().setFromPoints(drawingPoints);
+                drawingLines.push(new THREE.Line(drawingGeometry, drawingMaterial));
+                scene.add(drawingLines[drawingLines.length - 1]);
+                if (shapeSelector.selectedIndex === 1) {
+                    if (reticleVisible && flowersGltf) {
+                        const flower =
+                            flowersGltf.children[
+                            Math.floor(Math.random() * flowersGltf.children.length)
+                            ];
+                        const mesh = flower.clone();
 
-    if (frame) {
-        const referenceSpace = renderer.xr.getReferenceSpace();
-        const session = renderer.xr.getSession();
-
-        if (hitTestSourceRequested === false) {
-            session.requestReferenceSpace("viewer").then(function (referenceSpace) {
-                session
-                    .requestHitTestSource({ space: referenceSpace })
-                    .then(function (source) {
-                        hitTestSource = source;
-                    });
-            });
-
-            session.addEventListener("end", function () {
-                hitTestSourceRequested = false;
-                hitTestSource = null;
-            });
-
-            hitTestSourceRequested = true;
-        }
-
-        if (hitTestSource) {
-            const hitTestResults = frame.getHitTestResults(hitTestSource);
-
-            if (hitTestResults.length) {
-                if (!planeFound) {
-                    planeFound = true;
+                        reticleMatrix.decompose(mesh.position, mesh.quaternion, mesh.scale);
+                        scene.add(mesh);
+                    }
                 }
-                const hit = hitTestResults[0];
-
-                reticle.visible = true;
-                reticle.matrix.fromArray(hit.getPose(referenceSpace).transform.matrix);
-            } else {
-                reticle.visible = false;
+            }
+            if (hand.joints?.["index-finger-tip"]?.position != null) {
+                const x = hand.joints["index-finger-tip"].position.x;
+                const y = hand.joints["index-finger-tip"].position.y;
+                const z = hand.joints["index-finger-tip"].position.z;
+                if (x != lastPosition.x || y != lastPosition.y || z != lastPosition.z) {
+                    console.log(hand);
+                    drawingPoints.push(new THREE.Vector3(x, y, z));
+                    drawingGeometry.dispose();
+                    drawingGeometry = new THREE.BufferGeometry().setFromPoints(drawingPoints);
+                    drawingLines[drawingLines.length - 1].geometry = drawingGeometry;
+                    lastPosition = { x, y, z };
+                }
             }
         }
     }
-
+    else {
+        pinching = false;
+    }
     renderer.render(scene, camera);
 });
